233f309b80789e95c88bee6bcf3dbe9c9f7db1ca
Student. Hyenogmo.Kim <kaowltus12@ajou.ac.kr>
2022-01-20 03:26:29 +0900

    test profiling

---
+++ practice_self/fragment.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "circum.h"
+
+void float_to_int(float a)
+{
+        int b;
+        memcpy(&b, &a, sizeof(a));
+
+        int i;
+        int e;
+        unsigned int m;
+        i = b >> 31;
+        e = (b >> 23) & 255;
+        m = b << 9;
+        m >>= 9;
+        //m을 구할 때 &연산이 빠를지, 아니면 unsigned치환 후 bit연산이 빠를지 질문할 것.
+        printf("i : %d / e : %d / m : %d\n",i,e,m);
+
+        for (int u = 22; u >= 0; u--)
+        {
+                fprintf(stdout,"%d", ((m >> u) & 1));
+                if(u%4 == 0)
+                        fprintf(stdout," ");
+        }
+        fprintf(stdout, "\n");
+
+        fprintf(stdout, "%f\n",a);
+
+}
--- practice_self/type.c
+++ practice_self/type.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+
+void sumi(int ia)
+{
+	ia = 1 * 1;
+}
+	
+void sumf(float fa)
+{
+	fa = 1.0 * 1.0;
+}
+
 int main()
 {
-	short i = 32*32+32*32;
-	printf("%d",i);
+	int ia = 0;
+	float fa = 0.0;
+	for(int i = 0;i<1000000000;i++)
+	{
+		sumi(ia);
+		sumf(fa);
+	}
+
 }
